915a51b67586421b2cf296d6b56c1dfdc9483b50
sakibsarkar <sakibsarkar707@gmail.com>
2024-08-28 13:05:17 +0600

    conntection update

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
+import mongoose from "mongoose";
 import app from "./app";
-import connect from "./config/db";
 
 const port = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
-    connect();
-    console.log("Connected to MongoDB");
+    await mongoose.connect(process.env.MONGO_URI as string);
+    console.log("Connection Created to Mongodb");
 
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);

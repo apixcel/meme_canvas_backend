b700e025fc19af497f5af0cbcaa627ec97340de6
sakibsarkar <sakibsarkar707@gmail.com>
2024-08-28 13:00:25 +0600

    db conntection update

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   try {
-    mongoose.set("strictQuery", false);
     await mongoose.connect(process.env.MONGO_URI as string);
     console.log("Connection Created to Mongodb");
   } catch (error) {
